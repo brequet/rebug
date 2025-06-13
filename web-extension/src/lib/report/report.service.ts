7b6189153c18ef6eb55b018121bf3a0500bcda31
brequet <requet.baptiste@gmail.com>
2025-06-13 22:52:37 +0200

    send video report

--- web-extension/src/lib/report/report.service.ts
+++ web-extension/src/lib/report/report.service.ts
@@ -1,77 +1,73 @@
-import { API_BASE_URL } from "$lib/api"
-import { AuthUtils } from "$lib/auth/auth.utils"
-import { SCREENSHOT_MIME_TYPE, VIDEO_CAPTURE_MIME_TYPE } from "$lib/messaging/types"
-import { base64ToBlob } from "$lib/utils/blob-utils"
-import { ReportResponse, SendReportPayload } from "./report.types"
+import { API_BASE_URL } from '$lib/api';
+import { AuthUtils } from '$lib/auth/auth.utils';
+import { SCREENSHOT_MIME_TYPE, VIDEO_CAPTURE_MIME_TYPE } from '$lib/messaging/types';
+import { base64ToBlob } from '$lib/utils/blob-utils';
+import { ReportResponse, SendReportPayload } from './report.types';
 
 class ReportService {
-    private static readonly ENDPOINT = `${API_BASE_URL}/reports`
+	private static readonly ENDPOINT = `${API_BASE_URL}/reports`;
 
-    static async sendReport(report: SendReportPayload): Promise<ReportResponse | null> {
-        if (report.mediaType === "Screenshot") {
-            return this.sendScreenshot(report);
-        } else if (report.mediaType === "Video") {
-            throw new Error("Video reports are not supported yet.");
-        }
+	static async sendReport(report: SendReportPayload): Promise<ReportResponse | null> {
+		const authHeader = await AuthUtils.getAuthHeader();
+		if (!authHeader) {
+			return null;
+		}
 
-        return null
-    }
+		const formData = new FormData();
+		formData.append('board_id', report.boardId);
+		formData.append('title', report.title);
 
-    private static async sendScreenshot(report: SendReportPayload): Promise<ReportResponse | null> {
-        const authHeader = await AuthUtils.getAuthHeader()
-        if (!authHeader) {
-            return null;
-        }
+		if (report.description) {
+			formData.append('description', report.description);
+		}
 
-        const formData = new FormData();
-        formData.append("board_id", report.boardId);
-        formData.append("title", report.title);
+		if (report.originUrl) {
+			formData.append('url', report.originUrl);
+		}
 
-        if (report.description) {
-            formData.append("description", report.description);
-        }
+		if (report.browserInfo) {
+			formData.append('browser_name', report.browserInfo.name);
+			formData.append('browser_version', report.browserInfo.version);
+		}
 
-        if (report.originUrl) {
-            formData.append("url", report.originUrl);
-        }
+		if (report.os) {
+			formData.append('os_name', report.os);
+		}
 
-        if (report.browserInfo) {
-            formData.append("browser_name", report.browserInfo.name);
-            formData.append("browser_version", report.browserInfo.version);
-        }
+		const file = base64ToBlob(
+			report.mediaData,
+			report.mediaType === 'Screenshot' ? SCREENSHOT_MIME_TYPE : VIDEO_CAPTURE_MIME_TYPE
+		);
+		if (file) {
+			formData.append(
+				'file',
+				file,
+				report.mediaType === 'Screenshot' ? 'screenshot.png' : 'video.webm'
+			);
+		} else {
+			console.error('File is not a Blob:', typeof report.mediaData);
+			return null;
+		}
 
-        if (report.os) {
-            formData.append("os_name", report.os);
-        }
+		const url = `${this.ENDPOINT}`;
+		const headers = {
+			Authorization: authHeader
+		};
 
-        const file = base64ToBlob(report.mediaData, report.mediaType === "Screenshot" ? SCREENSHOT_MIME_TYPE : VIDEO_CAPTURE_MIME_TYPE);
-        if (file) {
-            formData.append("file", file, "screenshot.png");
-        } else {
-            console.error("File is not a Blob:", typeof report.mediaData);
-            return null;
-        }
+		const response = await fetch(url, {
+			method: 'POST',
+			headers: headers,
+			body: formData
+		});
 
-        const url = `${this.ENDPOINT}/screenshots`
-        const headers = {
-            Authorization: authHeader
-        }
+		if (!response.ok) {
+			console.error('Failed to send report:', response.statusText);
+			return null;
+		}
 
-        const response = await fetch(url, {
-            method: "POST",
-            headers: headers,
-            body: formData
-        })
-
-        if (!response.ok) {
-            console.error("Failed to send screenshot report:", response.statusText);
-            return null;
-        }
-
-        const data: ReportResponse = await response.json();
-        return data;
-    }
+		const data: ReportResponse = await response.json();
+		return data;
+	}
 }
 
-export { ReportService }
-
+export { ReportService };
